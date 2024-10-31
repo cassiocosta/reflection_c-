6c1c8b9fd879df632fa2704e17536c0ff6b8d380
Cássio h Costa <cassio@prouserapps.com>
2024-10-31 10:34:36 -0300

    first commit with readme and list Proprierties and methods examples

---
+++ examples/ListPropertiesAndMethods/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using ListPropertiesAndMethods;
+
+Type type = typeof(Car);
+
+Console.WriteLine("Properties:");
+foreach (var prop in type.GetProperties())
+{
+    Console.WriteLine($"- {prop.Name} ({prop.PropertyType})");
+}
+
+Console.WriteLine("\nMethods:");
+foreach (var method in type.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly))
+{
+    Console.WriteLine($"- {method.Name}");
+}
---
+++ examples/ListPropertiesAndMethods/readme.md
@@ -0,0 +1,12 @@
+# List Properties and Methods
+
+This folder contains an example of using Reflection in C# to list the properties and methods of a class (`Car`). This can be useful when you need to inspect an object's structure at runtime.
+
+## How to Run
+
+1. Open this folder in your IDE.
+2. Run `Program.cs`.
+
+You should see output listing the properties and methods of the `Car` class.
+
+
